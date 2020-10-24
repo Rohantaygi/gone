f90f5e8c222f6630d1c681e965887b54387ea24a
Rohantaygi <73010249+Rohantaygi@users.noreply.github.com>
2020-10-24 10:56:42 +0530

    Create astrologers star.py

---
+++ astrologers star.py
@@ -0,0 +1,23 @@
+import time
+
+while True:
+    a = int(input('type any number here: '))
+    if a%2 == 0:
+        print('this is an even number')
+
+        print('  ')
+
+        print('****')
+        print('***')
+        print('**')
+        print('*')
+    else:
+        print('this is an odd number')
+
+        print('  ')
+
+        print('*')
+        print('**')
+        print('***')
+        print('****')
+
